0baa790dab32caedb8a7dc04b851f4e56cb1b6e5
rahuldashetty <111636052+rahuldashetty@users.noreply.github.com>
2023-01-29 16:04:53 +0530

    Update lambda_s3_sns.py

--- lambda_s3_sns.py
+++ lambda_s3_sns.py
@@ -1,7 +1,7 @@
 import boto3
 
 def lambda_handler(event, context):
-    # Get the S3 bucket name and object key from the event
+    # Get the S3 bucket name and object name from the event
     bucket_name = event["Records"][0]["s3"]["bucket"]["name"]
     object_key = event["Records"][0]["s3"]["object"]["key"]
     
